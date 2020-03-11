be9661198ee823e1819b17812dcf2d7c4e32e125
Zsofia <oroszi.zsofi@gmail.com>
2020-03-11 13:00:06 +0100

    Add form with change handler for new Todos

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
 
 class NewTodoForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {newTodo: ""};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(evt){
+        this.setState({ [evt.target.name]: evt.target.value });
+    }
+
     render(){
-        return <h1>Hello from NewTodoForm</h1>
+        return (
+            <form>
+                <label htmlFor="newTodo">To do: </label>
+                <input 
+                    type="text"
+                    name="newTodo" 
+                    id="nwTodo" 
+                    value={this.state.newTodo} 
+                    onChange={this.handleChange}>
+                </input>
+                <button>Add</button>
+            </form>
+        )
     }
 }
 
